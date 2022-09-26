17fbb80c89c471f217f5543a1d10a0bca2412158
Ednilson Martins <juniorzmartin@gmail.com>
2022-09-26 18:51:19 -0300

    Modificando a exibição com base no argumento

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import App from './App.vue'
 const Vue = createApp(App)
 Vue.directive('texto', {
     created(el, binding){  // chamaddo antes que os atributos do elemento ou ouvintes de event(event listeners) sejam aplicados
-    
         if(binding.value?.cor) el.style.color = binding.value.cor
         if(binding.value?.tamanhoFonte) el.style.fontSize = binding.value.tamanhoFonte
 
@@ -41,7 +40,9 @@ Vue.directive('posicao', {
 Vue.directive('informacao',{
     created(el, binding){
         console.log(el,binding.arg, binding.modifiers, binding.value)
-        let funcao = function () {
+        let funcao = null
+        if(binding.arg == undefined || binding.arg === 'simples'){
+         funcao = function () {
             let informacaoSpan = document.createElement('span')
             informacaoSpan.style.position = 'absolute'
             informacaoSpan.style.background = '#ddd'
@@ -55,11 +56,41 @@ Vue.directive('informacao',{
                 informacaoSpan.remove()
             })
         }
+    }
+    if(binding.arg === 'destacado'){
+        funcao = function() { 
+            let informacaoDivContainer = document.createElement('div')
+            let informacaoDiv = document.createElement('div')
+            informacaoDiv.innerText = binding.value
+            informacaoDivContainer.style.width = '100%'
+            informacaoDivContainer.style.height = '100%'
+            informacaoDivContainer.style.display = 'flex'
+            informacaoDivContainer.style.flexDirection = 'row'
+            informacaoDivContainer.style.alignItems = 'center'
+            informacaoDivContainer.style.justifyContent = 'center'
+            informacaoDivContainer.style.background = '#ccc'
+            informacaoDivContainer.style.position = 'absolute'
+            informacaoDivContainer.style.top = '0'
+            informacaoDivContainer.style.zIndex = '10'
+
+            informacaoDiv.style.padding = '30px'
+            informacaoDiv.style.background = '#fba'
+
+            informacaoDivContainer.appendChild(informacaoDiv)
+
+            el.appendChild(informacaoDivContainer)
+
+            informacaoDivContainer.addEventListener('click', (event) => {
+                event.stopPropagation()
+                informacaoDivContainer.remove()
+            })
+    }
+    }
         if(binding.modifiers['umClickMouse']){
             el.addEventListener('click', funcao)
         }
-        if(binding.modifiers['doisClicksMouse']){
-            el.addEventListener('dblclick', funcao)
+        if(binding.modifiers['oneClicksMouse']){
+            el.addEventListener('click', funcao)
         }
     }
 })
