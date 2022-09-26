56db3d8fcdb92d4b3b0e31de78e4727c1fcb003e
Ednilson Martins <juniorzmartin@gmail.com>
2022-09-26 07:20:04 -0300

    testando: Binding de valores da diretiva

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <p>Sem diretiva</p>
-  <p v-texto>Primeira diretiva customizada</p>
-  <p v-texto>testando</p>
+  <p v-texto="{cor: 'blue', tamanhoFonte: '200%', totalCaracteres:15}">Primeira diretiva customizada</p>
+  <p v-texto="{cor: 'red', tamanhoFonte:'300%', totalCaracteres:11}">testando v-texto 2</p>
+  <p v-texto="{cor: 'green', tamanhoFonte:'400%', totalCaracteres:4}">testando v-texto 3</p>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -3,23 +3,27 @@ import App from './App.vue'
 
 const Vue = createApp(App)
 Vue.directive('texto', {
-    created(el){  // chamaddo antes que os atributos do elemento ou ouvintes de event(event listeners) sejam aplicados
-        console.log(el.style)
-        el.style.color = 'red' 
-        el.style.fontSize= '150%'
+    created(el, binding){  // chamaddo antes que os atributos do elemento ou ouvintes de event(event listeners) sejam aplicados
+    
+        if(binding.value?.cor) el.style.color = binding.value.cor
+        if(binding.value?.tamanhoFonte) el.style.fontSize = binding.value.tamanhoFonte
 
+        let totalCaracteres = 25
+        if(binding.value?.totalCaracteres) totalCaracteres = binding.value.totalCaracteres
+      
         let textoOriginal = el.innerText
         let tamanhoTextoOriginal = textoOriginal.length
         let textoAjustado = ''
 
-        if(tamanhoTextoOriginal > 25){
+        if(tamanhoTextoOriginal > totalCaracteres){
             // truncar o texto em 22 caracteres e adicionar '...'
-            textoAjustado = textoOriginal.substring(0,22) + '...' 
+            textoAjustado = textoOriginal.substring(0,(totalCaracteres - 3)) + '...' 
         }else{
             // manter o texto original
             textoAjustado = textoOriginal
         }
         el.innerText = textoAjustado
+
     }
 })
 Vue.mount('#app')
