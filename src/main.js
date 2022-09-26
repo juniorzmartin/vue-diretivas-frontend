93c0d836cadd3ec72d942832106866155eb01892
Ednilson Martins <juniorzmartin@gmail.com>
2022-09-26 05:43:51 -0300

    testando: criando diretiva personalizada

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const Vue = createApp(App)
+Vue.directive('teste', {
+    created(){  // chamaddo antes que os atributos do elemento ou ouvintes de event(event listeners) sejam aplicados
+        console.log("A diretivas foi aplicada")
+    }
+})
+Vue.mount('#app')
